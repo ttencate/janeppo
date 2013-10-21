65b41b16fdb1c35d5451ae11020920b13d93bb4c
Erik Weitenberg <eweitenberg@gmail.com>
2013-10-21 14:25:34 +0200

    gofmt

--- configedit.go
+++ configedit.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-    "fmt"
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 )
 
 type Config struct {
-    Nickname string
-    Server string
-    Channel string
-    Quotefile string
-    UrlLength int
-    Verbose bool
+	Nickname  string
+	Server    string
+	Channel   string
+	Quotefile string
+	UrlLength int
+	Verbose   bool
 }
 
 func main() {
-    fmt.Println("This tool will help you create a config file for Janeppo.")
-    confFile := GetString("File to save to, press enter for default")
-    if confFile == "" {
-        confFile = "config.json"
-    }
-    conf := Config{
-        Nickname:  GetString("Nickname for the bot"),
-        Server:    GetString("IRC server, url:port"),
-        Channel:   GetString("IRC channel, including '#'"),
-        Quotefile: GetString("Filename of quote database"),
-        UrlLength: GetInt("Length of an url above which the bot will generate a short url"),
-        Verbose:   GetBool("Verbose logging"),
-    }
-    jsonBlob, err := json.Marshal(conf)
-    if err != nil {
-        fmt.Println("\nCannot make config file, ", err)
-    } else {
-        ioutil.WriteFile(confFile, jsonBlob, 0640)
-        fmt.Println("\nConfig written to " + confFile + ".")
-    }
+	fmt.Println("This tool will help you create a config file for Janeppo.")
+	confFile := GetString("File to save to, press enter for default")
+	if confFile == "" {
+		confFile = "config.json"
+	}
+	conf := Config{
+		Nickname:  GetString("Nickname for the bot"),
+		Server:    GetString("IRC server, url:port"),
+		Channel:   GetString("IRC channel, including '#'"),
+		Quotefile: GetString("Filename of quote database"),
+		UrlLength: GetInt("Length of an url above which the bot will generate a short url"),
+		Verbose:   GetBool("Verbose logging"),
+	}
+	jsonBlob, err := json.Marshal(conf)
+	if err != nil {
+		fmt.Println("\nCannot make config file, ", err)
+	} else {
+		ioutil.WriteFile(confFile, jsonBlob, 0640)
+		fmt.Println("\nConfig written to " + confFile + ".")
+	}
 }
 
 func GetString(prompt string) (result string) {
-    fmt.Print(prompt + " :")
-    fmt.Scanln(&result)
-    return
+	fmt.Print(prompt + " :")
+	fmt.Scanln(&result)
+	return
 }
 func GetInt(prompt string) (result int) {
-    fmt.Print(prompt + " #")
-    fmt.Scanf("%d", &result)
-    return
+	fmt.Print(prompt + " #")
+	fmt.Scanf("%d", &result)
+	return
 }
 func GetBool(prompt string) (result bool) {
-    fmt.Print(prompt + " (true|false):")
-    fmt.Scanf("%t", &result)
-    return
+	fmt.Print(prompt + " (true|false):")
+	fmt.Scanf("%t", &result)
+	return
 }
